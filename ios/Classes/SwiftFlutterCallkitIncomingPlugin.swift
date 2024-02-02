3d3be9fd89dad049d917f07a7ecaef4ddb248c10
kucinskasjustas <justas.kucinskas@okinos.co>
2024-02-02 18:52:15 +0200

    Calls: add self reference

--- ios/Classes/SwiftFlutterCallkitIncomingPlugin.swift
+++ ios/Classes/SwiftFlutterCallkitIncomingPlugin.swift
@@ -217,7 +217,7 @@ public class SwiftFlutterCallkitIncomingPlugin: NSObject, FlutterPlugin, CXProvi
         connectedPlayer?.play()
 
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.1) {
-            configurAudioSession()
+            self.configurAudioSession()
             RTCAudioSession.sharedInstance().audioSessionDidActivate(self.activatedAVAudioSession!)
             RTCAudioSession.sharedInstance().isAudioEnabled = true
         }
